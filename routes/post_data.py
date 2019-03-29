31de07cc5e1fb017310bee4fca35ad5621c2ea19
mjesar <mohammadalijaisar@gmail.com>
2019-03-29 15:17:44 +0500

    empty post_data() method added to get data from json and convert it to csv

--- routes/post_data.py
+++ routes/post_data.py
@@ -4,3 +4,8 @@ from flask import Blueprint,jsonify,request
 post_api = Blueprint('post_api', __name__)
 
 @post_api.route('/data/api/v1.0/input',methods=['POST'])
+def post_data():
+
+
+    
+    return jsonify({'': ""})
