d4f38c5e1454770535c09040734e0a05399172a5
mjesar <mohammadalijaisar@gmail.com>
2019-03-30 01:06:48 +0500

    class with stracture of data and create.db once used to create database file once

--- databaseModel.py
+++ databaseModel.py
@@ -1,5 +1,13 @@
 #Using SQLAlchemy to store data in local system
 from flask_sqlalchemy import SQLAlchemy
 
-#database object 
+#database object
 db = SQLAlchemy
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    lastname=db.Column(db.String(20))
+
+
+db.create_all()
