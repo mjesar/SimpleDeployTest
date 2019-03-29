63c50cc985b7126620d7075104d3cf34dcafb68b
mjesar <mohammadalijaisar@gmail.com>
2019-03-29 19:57:22 +0500

    Writing to csv file with colmns first_name and last name and with asiging index using pandas dataframe

--- routes/post_data.py
+++ routes/post_data.py
@@ -1,5 +1,8 @@
 from flask import Blueprint,jsonify,request
 import csv
+import pandas as pd
+
+
 
 post_api = Blueprint('post_api', __name__)
 
@@ -8,52 +11,16 @@ def post_data():
 
     data = request.get_json(force=True)
 
-    id=data["id"]
-    first_name = data["first_name"]
-    last_name = data["last_name"]
-
-    people_data = open('people.csv', 'w')
-
-    csvwriter = csv.writer(people_data)
-
-    count = 0
-
-    for emp in data:
-
-        if count == 0:
-            header = emp
-
-            csvwriter.writerow(header)
-
-            count += 1
-
-        csvwriter.writerow(emp)
-
-    people_data.close()
-
-    # csv_columns = ['No', 'Name', 'Country']
-    # dict_data = [
-    #     {'id': 1, 'Name': 'Alex', 'Country': 'India'},
-    #     {'No': 2, 'Name': 'Ben', 'Country': 'USA'},
-    #     {'No': 3, 'Name': 'Shri Ram', 'Country': 'India'},
-    #     {'No': 4, 'Name': 'Smith', 'Country': 'USA'},
-    #     {'No': 5, 'Name': 'Yuva Raj', 'Country': 'India'},
-    # ]
-    # csv_file = "Names.csv"
-    # try:
-    #     with open("people.csc", 'w') as csvfile:
-    #         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-    #         writer.writeheader()
-    #         for data in dict_data:
-    #             writer.writerow(data)
-    # except IOError:
-    #     print("I/O error")
 
 
-        # with open('people.csv', 'w') as csvfile:
-    #     filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #     filewriter.writerow(['ID', 'First Name', "Last Name"])
-    #
-    #     filewriter.writerow([id, first_name,last_name])
+    first_name = data['first_name']
+    last_name = data['last_name']
+
+    dict = {}
+    dict.update({"First name": [first_name], "Last Name": [last_name]})
+    df = pd.DataFrame(dict)
+    with open("people.csv", 'a') as f:
+        df.to_csv(f, mode='a', index=False, header=f.tell() == 0)
+
 
     return jsonify({'InputedData': data})
