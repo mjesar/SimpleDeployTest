62ad626c4823b03f48dc67a2149457ada0a2d08b
mjesar <mohammadalijaisar@gmail.com>
2019-03-29 22:38:17 +0500

    autoincreament Id read the lenght of complete index then increase it by 1

--- routes/post_data.py
+++ routes/post_data.py
@@ -16,11 +16,14 @@ def post_data():
     first_name = data['first_name']
     last_name = data['last_name']
 
+    count_id = pd.read_csv("people.csv")
+    len_index=len(count_id['ID'])
+    print(len_index+1)
+
     dict = {}
-    dict.update({"First name": [first_name], "Last Name": [last_name]})
+    dict.update({"ID": [len_index+1], "First name": [first_name], "Last Name": [last_name]})
     df = pd.DataFrame(dict)
     with open("people.csv", 'a') as f:
         df.to_csv(f, mode='a', index=False, header=f.tell() == 0)
-
 
     return jsonify({'InputedData': data})
