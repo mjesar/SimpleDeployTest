a78390cf7dfe28ada214e05a394e95dc704535da
mjesar <mohammadalijaisar@gmail.com>
2019-03-30 01:20:02 +0500

    configure SQLAlchemy database name as people.db and initilized it to locally save data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response
 
+from databaseModel import db
 
 from routes.post_data import post_api
 
@@ -7,6 +8,12 @@ from routes.post_data import post_api
 app = Flask(__name__)
 
 
+# configure SQLALCHEMY
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aliali@localhost/people'
+
+# initialized it with db object
+db.init_app(app)
 
 
 app.register_blueprint(post_api)
