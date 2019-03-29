d2ae8c9f1d13bda0c116da111d219725beb37725
mjesar <mohammadalijaisar@gmail.com>
2019-03-29 15:21:47 +0500

    data object to get data in json

--- routes/post_data.py
+++ routes/post_data.py
@@ -6,6 +6,8 @@ post_api = Blueprint('post_api', __name__)
 @post_api.route('/data/api/v1.0/input',methods=['POST'])
 def post_data():
 
+    data = request.get_json()
 
-    
+
+
     return jsonify({'': ""})
